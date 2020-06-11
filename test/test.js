7075dc2b82aff1ea3b9420ea578ffdc649f6fcc3
Deepika <bhuvanadeepika017@gmail.com>
2020-06-11 10:53:14 +0530

    added test cases

--- test/test.js
+++ test/test.js
@@ -2,23 +2,31 @@ const assert = require('chai').assert;
 const { bfs } = require("../src/graph");
 
 describe("BFS", function() {
-  const pairs = [["India", "Pakistan"], ["India", "Nepal"], ["India", "China"], ["Nepal", "China"], ["Pakistan", "China"]];
+  const pairs = [["India", "Pakistan"], ["India", "Nepal"], ["India", "China"], ["Nepal", "China"], ["Pakistan", "China"], ["China", "Russia"]];
   it("should give true if two node connected perfectly", function() {
-    assert.ok(bfs(pairs, "India", "Pakistan"))
+    assert.ok(bfs(pairs, "India", "Pakistan"));
   })
+
+  it("should give true if two node connected moderately", function() {
+    assert.ok(bfs(pairs, "India", "Russia"));
+  })
+
   it("should give false if source has no path to target", function() {
-    assert.notOk(bfs(pairs, "china", "Pakistan"))
+    assert.notOk(bfs(pairs, "china", "Pakistan"));
   })
+
   it("should give true for single node connected to itself", function() {
     const routes = [["aa", "aa"]];
-    assert.ok(bfs(routes, "aa", "aa"))
+    assert.ok(bfs(routes, "aa", "aa"));
   })
+
   it("should give false for single node not connected to itself", function() {
     const routes = [["aa", "bb"]];
-    assert.notOk(bfs(routes, "aa", "aa"))
+    assert.notOk(bfs(routes, "aa", "aa"));
   })
+
   it("should give false for source node is not in the list", function() {
     const routes = [["aa", "bb"], ["aa", "cc"]];
-    assert.notOk(bfs(routes, "dd", "cc"))
+    assert.notOk(bfs(routes, "dd", "cc"));
   })
 });

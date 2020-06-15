976ee6abd42e738632980b23639a0a79307e3894
Deepika <bhuvanadeepika017@gmail.com>
2020-06-15 13:02:16 +0530

    wrote test cases for DFS function

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 const assert = require('chai').assert;
-const { bfs } = require("../src/graph");
+const { bfs, dfs } = require("../src/graph");
 
 describe("BFS", function() {
   const pairs = [["India", "Pakistan"], ["India", "Nepal"], ["India", "China"], ["Nepal", "China"], ["Pakistan", "China"], ["China", "Russia"]];
@@ -28,5 +28,35 @@ describe("BFS", function() {
   it("should give false for source node is not in the list", function() {
     const routes = [["aa", "bb"], ["aa", "cc"]];
     assert.notOk(bfs(routes, "dd", "cc"));
+  })
+});
+
+describe("DFS", function() {
+  const pairs = [["India", "Pakistan"], ["India", "Nepal"], ["India", "China"], ["Nepal", "China"], ["Pakistan", "China"], ["China", "Russia"]];
+  it("should give true if two node connected perfectly", function() {
+    assert.deepStrictEqual(dfs(pairs, "India", "Pakistan"), ["India", "Pakistan"]);
+  })
+
+  it("should give true if two node connected moderately", function() {
+    assert.deepStrictEqual(dfs(pairs, "India", "Russia"), ["India", "China", "Russia"]);
+  })
+
+  it("should give false if source has no path to target", function() {
+    assert.isUndefined(dfs(pairs, "china", "Pakistan"));
+  })
+
+  it("should give true for single node connected to itself", function() {
+    const routes = [["aa", "aa"]];
+    assert.deepStrictEqual(dfs(routes, "aa", "aa"), ["aa", "aa"]);
+  })
+
+  it("should give false for single node not connected to itself", function() {
+    const routes = [["aa", "bb"]];
+    assert.isUndefined(dfs(routes, "aa", "aa"));
+  })
+
+  it("should give false for source node is not in the list", function() {
+    const routes = [["aa", "bb"], ["aa", "cc"]];
+    assert.isUndefined(dfs(routes, "dd", "cc"));
   })
 });
